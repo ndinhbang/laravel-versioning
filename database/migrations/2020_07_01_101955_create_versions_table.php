cc0df654a0036c98527334379e03b383791cda1b
Alec <hello@itsalec.co.uk>
2021-01-15 11:18:09 +0000

    Fix migration

--- database/migrations/2020_07_01_101955_create_versions_table.php
+++ database/migrations/2020_07_01_101955_create_versions_table.php
@@ -16,8 +16,15 @@ class CreateVersionsTable extends Migration
         Schema::create('versions', function (Blueprint $table) {
             $table->increments('id');
             $table->morphs('versionable');
-            $table->bigInteger('user_id')->nullable()->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');
+
+            $userIdColType = Schema::getColumnType('users', 'id');
+
+            if ($userIdColType == 'integer') {
+                $table->unsignedInteger('user_id');
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');
+            } else {
+                $table->foreignId('user_id')->constrained()->onDelete('SET NULL');
+            }
             $table->integer('relation_id')->nullable()->unsigned();
             $table->foreign('relation_id')->references('id')->on('versions')->onDelete('cascade');
 			$table->binary('model_data');
